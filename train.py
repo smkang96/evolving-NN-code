6a9ce06754162aefa5949fdb49a63e5a62bcb343
Sungmin <stuatlittle@kaist.ac.kr>
2018-11-11 13:02:07 +0900

    added preliminary training code

---
+++ train.py
@@ -0,0 +1,60 @@
+'''Training code to evaluate NNs'''
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch.backends import cudnn
+cudnn.benchmark = True
+import torchvision
+import torchvision.transforms as transforms
+
+## Get Model Architecture /TODO: import from various sources of code
+import mobilenet_model
+
+## Data Loaders
+# from pytorch tutorial
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False, num_workers=2)
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+## Network and Optimizer
+net = mobilenet_model.Net()
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+
+## Training: from pytorch tutorial
+for epoch in range(2):  # loop over the dataset multiple times
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 50 == 49:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 50))
+            running_loss = 0.0
+
+print('Finished Training')
