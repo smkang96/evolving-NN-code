8a4d60d4c24e1a1d6f9bbc881d8eba506027e1df
David Kim <dafakim@gmail.com>
2018-11-11 21:17:32 +0900

    removed notes

--- translated_nncode/alexnet_model.py
+++ translated_nncode/alexnet_model.py
@@ -25,27 +25,6 @@ class AlexClassifier(nn.Module):
             nn.Linear(4096, num_classes),
         )
 
-nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
-nn.ReLU(inplace=True),
-
-nn.MaxPool2d(kernel_size=3, stride=2),
-
-nn.Conv2d(64, 192, kernel_size=5, padding=2),
-nn.ReLU(inplace=True),
-
-nn.MaxPool2d(kernel_size=3, stride=2),
-
-nn.Conv2d(192, 384, kernel_size=3, padding=1),
-nn.ReLU(inplace=True),
-
-nn.Conv2d(384, 256, kernel_size=3, padding=1),
-nn.ReLU(inplace=True),
-
-nn.Conv2d(256, 256, kernel_size=3, padding=1),
-nn.ReLU(inplace=True),
-
-nn.MaxPool2d(kernel_size=3, stride=2),
-
 class Net(nn.Module):
 	def __init__(self):
 		super(Net, self).__init__()
