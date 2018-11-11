58b608b77a00076e66b13dccedbae5e4c2b5303a
David Kim <dafakim@gmail.com>
2018-11-11 21:13:04 +0900

    alexnet updated with forward specifications

--- translated_nncode/alexnet_model.py
+++ translated_nncode/alexnet_model.py
@@ -2,59 +2,71 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class Alexlayer(nn.Module):
+class AlexBlock(nn.Module):
+	def __init__(self, inp, outp, k_size, stride=1, padding):
+		self.block = nn.Sequential(
+			nn.Conv2d(inp, outp, kernel_size = k_size, stride = stride, padding=padding),
+			nn.ReLU(inplace=True),
+		)
 
+	def forward(self, x):
+		out = self.block(x)
+		return x
+
+class AlexClassifier(nn.Module):
 	def __init__(self, num_classes=1000):
-		self.conv1 = nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2)
-		self.relu = nn.ReLU(inplace=True)
-		self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2)
-        self.conv2 = nn.Conv2d(64, 192, kernel_size=5, padding=2)
-        #nn.ReLU(inplace=True),
-        #nn.MaxPool2d(kernel_size=3, stride=2),
-        self.conv3 = nn.Conv2d(192, 384, kernel_size=3, padding=1)
-        #nn.ReLU(inplace=True),
-        self.conv4 = nn.Conv2d(384, 256, kernel_size=3, padding=1)
-        #nn.ReLU(inplace=True),
-        self.conv5 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
-        #nn.ReLU(inplace=True),
-        #nn.MaxPool2d(kernel_size=3, stride=2)
-        self.dropout = nn.Dropout()
-        self.linear1 = self.Linear(256 * 6 * 6, 4096)
-        #nn.ReLU
-        #nn.dropout
-        self.linear2 = self.linear(4096, 4096)
-        #nn.Relu
-        self.linear3 = self.linear(4096, num_classes)
+		self.classifier = nn.Sequential(
+			nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
 
-    def forward(self, x):
-    	x = self.conv1(x)
-    	out = self.relu(out)
-    	out = self.maxpool(out)
-    	out = self.conv2(out)
-    	out = self.relu(out)
-    	out = self.maxpool(out)
-    	out = self.conv3(out)
-    	out = self.relu(out)
-    	out = self.conv4(out)
-    	out = self.relu(out)
-    	out = self.conv5(out)
-    	out = self.relu(out)
-    	out = self.maxpool(out)
-    	out = self.dropou(out)
-    	out = self.linear(out)
-    	out = self.relu(out)
-    	out = self.dropout(out)
-    	out = self.linear2(out)
-    	out = self.relu(out)
-    	out = self.linear3(out)
-    	return out
+nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+nn.ReLU(inplace=True),
+
+nn.MaxPool2d(kernel_size=3, stride=2),
+
+nn.Conv2d(64, 192, kernel_size=5, padding=2),
+nn.ReLU(inplace=True),
+
+nn.MaxPool2d(kernel_size=3, stride=2),
+
+nn.Conv2d(192, 384, kernel_size=3, padding=1),
+nn.ReLU(inplace=True),
+
+nn.Conv2d(384, 256, kernel_size=3, padding=1),
+nn.ReLU(inplace=True),
+
+nn.Conv2d(256, 256, kernel_size=3, padding=1),
+nn.ReLU(inplace=True),
+
+nn.MaxPool2d(kernel_size=3, stride=2),
 
 class Net(nn.Module):
 	def __init__(self):
 		super(Net, self).__init__()
 
-		self.layer = Alexlayer()
+		self.layer1 = AlexBlock(3, 64, 11, 4, 2)
+		self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2)
+		self.layer2 = AlexBlock(64, 192, 5, padding=2)
+		self.layer3 = AlexBlock(192, 384, 3, padding=1)
+		self.layer4 = AlexBlock(384, 256, 3, padding=1)
+		self.layer5 = AlexBlock(256, 256, 3, padding=1)
+		#number of classes is 1000?
+		self.classifier = AlexClassifier(1000)
 
 	def forward(self, x):
-		out = self.layer(x)
+		out = self.layer1(x)
+		out = self.maxpool(out)
+		out = self.layer2(out)
+		out = self.maxpool(out)
+		out = self.layer3(out)
+		out = self.layer4(out)
+		out = self.layer5(out)
+		out = self.maxpool(out)
+		out = self.classifier(out)
 		return out
