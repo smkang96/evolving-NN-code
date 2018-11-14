b7243185da617df1d22fa3fb2bdcfdf2e4eaa3aa
Sungmin <stuatlittle@kaist.ac.kr>
2018-11-14 18:56:09 +0900

    bug fix

--- translated_nncode/ACGAN_D_model.py
+++ translated_nncode/ACGAN_D_model.py
@@ -26,12 +26,12 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         
-        self.conv1 = ConvDrop_ACGAN(3, 16, 3, 2, 1, bn=False)
-        self.conv2 = ConvDrop_ACGAN(16, 32, 3, 1, 0)
-        self.conv3 = ConvDrop_ACGAN(32, 64, 3, 2, 1)
-        self.conv4 = ConvDrop_ACGAN(64, 128, 3, 1, 0)
-        self.conv5 = ConvDrop_ACGAN(128, 256, 3, 2, 1)
-        self.conv6 = ConvDrop_ACGAN(256, 512, 3, 1, 0)
+        self.conv1 = ConvDrop_ACGAN(3, 16, 2, 1, bn=False)
+        self.conv2 = ConvDrop_ACGAN(16, 32, 1, 0)
+        self.conv3 = ConvDrop_ACGAN(32, 64, 2, 1)
+        self.conv4 = ConvDrop_ACGAN(64, 128, 1, 0)
+        self.conv5 = ConvDrop_ACGAN(128, 256, 2, 1)
+        self.conv6 = ConvDrop_ACGAN(256, 512, 1, 0)
         self.fc = nn.Linear(512*13*13, 10)
 
     def forward(self, x):
