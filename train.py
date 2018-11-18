e29c7e2bbfe1698043751acd08d28b67890f8cee
Sungmin <stuatlittle@kaist.ac.kr>
2018-11-18 13:16:31 +0900

    eval code complete; encapsulated in function

--- train.py
+++ train.py
@@ -8,14 +8,12 @@ cudnn.benchmark = True
 import torchvision
 import torchvision.transforms as transforms
 
-## Get Model Architecture /TODO: import from various sources of code
-from translated_nncode.stupidnet import Net
+# from translated_nncode.stupidnet import Net
 
-import time
+import imp
 
 ## Evaluation settings
-allowed_train_time = 2 * 60 # in seconds
-
+allowed_train_time = 2 * 15 # in seconds
 
 ## Data Loaders
 # from pytorch tutorial
@@ -35,58 +33,56 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=16,
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-## Network and Optimizer
-net = Net()
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+def indiv_evaluator(filename):
+    import time # ok, really weird error
+    module = imp.load_source('module', filename) # imports net structure
 
-## Training: from pytorch tutorial
-train_start = time.time()
-for epoch in range(2):
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        # get the inputs
-        inputs, labels = data
+    ## Network and Optimizer
+    net = module.Net()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
 
-        # zero the parameter gradients
-        optimizer.zero_grad()
+    ## Training: from pytorch tutorial
+    print 'training...'
+    train_start = time.time()
+    for epoch in range(10):
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs
+            inputs, labels = data
 
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+            # zero the parameter gradients
+            optimizer.zero_grad()
 
-        # print statistics
-        running_loss += loss.item()
-        if i % 10 == 9:    # print every 2000 mini-batches
-            print('[%d|%5d] loss: %.3f, time left: %.3fs' %
-                  (epoch + 1, i + 1, running_loss / 10, train_start-time.time()+allowed_train_time))
-            running_loss = 0.0
-        
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            if time.time() - train_start > allowed_train_time:
+                break
         if time.time() - train_start > allowed_train_time:
             break
-    if time.time() - train_start > allowed_train_time:
-        break
 
-correct = 0
-total = 0
-start = time.time()
-with torch.no_grad():
-    for d_i, data in enumerate(testloader):
-        images, labels = data
-        outputs = net(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-        if d_i % 10 == 9:
-            print('working...')
-end = time.time()
+    correct = 0
+    total = 0
+    print 'evaluating...'
+    start = time.time()
+    with torch.no_grad():
+        for d_i, data in enumerate(testloader):
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    end = time.time()
 
-accuracy = 100. * correct / total
-time = end - start
-        
-print('Accuracy: %.3f %%' % (accuracy,))
-print('Time: %.3f %%' % (time,))
-            
-print('Finished Training')
+    accuracy = 100. * correct / total
+    time = end - start
+    return accuracy, time
+
+def evaluator(dirname, filenames):
+    results = []
+    for filename in filenames:
+        results.append(indiv_evaluator(dirname + filename))
+    return results
