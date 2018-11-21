386685efdf0f90d53b906cc5ab4499f75c49875b
Sungmin <stuatlittle@kaist.ac.kr>
2018-11-21 23:13:22 +0900

    GA code added. but status is... merong :P

--- GAcode/ga.py
+++ GAcode/ga.py
@@ -1,34 +1,70 @@
 from evaluation import Evaluator
 import crossover
-from mutation.parsing import mutation
+from mutation.parsing import mutation, no_mutation
+from MOEAII import NSGAII
 
 import random
 
 class Breeder(object):
-    def __init__(self, init_pop, growth_time=2*60, mut_prob=0.5):
+    def __init__(self, init_pop, growth_time=2*60, mut_prob=0.5, pop_size=30):
         self._init_pop = init_pop
-        self._evaluator = Evaluator(growth_time)
         self._mut_prob = mut_prob
+        self._evaluator = Evaluator(growth_time)
+        self._nsgaii_sorter = NSGAII(2, None, None)
+        self._pop_size = pop_size
         
-    def breed(self, gen_num, pop_num):
+    def breed(self, gen_num):
         curr_pop = self._init_pop[:] # copy
         for g_idx in range(gen_num):
-            for i_idx in range(pop_num-len(curr_pop)):
-                poppa = random.choice(curr_pop)
-                momma = random.choice(curr_pop)
-                kid = crossover.crossover(poppa, momma, '%d_%d' % (g_idx, i_idx))
-                if self._mut_prob > random.random():
-                    kid = mutation(kid)
-                curr_pop.append(kid)
+            kids = []
+            for i_idx in range(self._pop_size-len(curr_pop)):
+                try:
+                    poppa = random.choice(curr_pop)
+                    momma = random.choice(curr_pop)
+                    kid = crossover.crossover(poppa, momma, '%d_%d' % (g_idx, i_idx))
+                    print kid
+                    if self._mut_prob > random.random():
+                        kid = mutation(kid)
+                    kids.append(kid)
+                except Exception as e:
+                    print '%d_%d' % (g_idx, i_idx), 'dead'
+                    print 'death cause:', str(e)
+                    continue
+            curr_pop += kids
+            
+            for name in curr_pop:
+                print name
             
             eval_results = []
+            eval_dict = {} # for fun
             for indiv in curr_pop:
+                print indiv
                 try:
-                    indiv_score = self._evaluator.evaluate(indiv)
-                except:
-                    print 'indiv born with defect, killed'
+                    indiv_score = self._evaluator._indiv_evaluator(indiv)
+                except Exception as e:
+                    print 'indiv born with defect, killed.'
+                    print 'death cry:', str(e)
                     indiv_score = (-1, 10000000)
+                eval_dict[indiv] = indiv_score
+                indiv_score = (indiv, indiv_score[0], indiv_score[1])
                 eval_results.append(indiv_score)
-                    
-            
-        print curr_pop
+        
+            selected = self._nsgaii_sorter.run(eval_results, self._pop_size)
+        
+        for chosen in selected:
+            print 'Honorable chosen neural network %s' % chosen
+            print 'Score: (%.2f, %.2f)' % (eval_dict[chosen])
+        
+        # print curr_pop
+
+INIT_POP = [
+    '../translated_nncode/ACGAN_D_model.py', 
+    '../translated_nncode/alexnet_model.py',
+    '../translated_nncode/BayesianCNN.py',
+    '../translated_nncode/densenet.py',
+    '../translated_nncode/googlenet.py',
+    '../translated_nncode/mobilenet_model.py'
+]
+        
+b = Breeder(INIT_POP, growth_time = 15, pop_size = 15)
+b.breed(1)
