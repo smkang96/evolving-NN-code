f492d14fa163e999e66903ba35b13f5558ed2b21
Sungmin <stuatlittle@kaist.ac.kr>
2018-11-21 16:00:31 +0900

    ga update

--- GAcode/ga.py
+++ GAcode/ga.py
@@ -5,9 +5,10 @@ from mutation.parsing import mutation
 import random
 
 class Breeder(object):
-    def __init__(self, init_pop, growth_time=2*60,):
+    def __init__(self, init_pop, growth_time=2*60, mut_prob=0.5):
         self._init_pop = init_pop
         self._evaluator = Evaluator(growth_time)
+        self._mut_prob = mut_prob
         
     def breed(self, gen_num, pop_num):
         curr_pop = self._init_pop[:] # copy
@@ -16,4 +17,18 @@ class Breeder(object):
                 poppa = random.choice(curr_pop)
                 momma = random.choice(curr_pop)
                 kid = crossover.crossover(poppa, momma, '%d_%d' % (g_idx, i_idx))
+                if self._mut_prob > random.random():
+                    kid = mutation(kid)
+                curr_pop.append(kid)
+            
+            eval_results = []
+            for indiv in curr_pop:
+                try:
+                    indiv_score = self._evaluator.evaluate(indiv)
+                except:
+                    print 'indiv born with defect, killed'
+                    indiv_score = (-1, 10000000)
+                eval_results.append(indiv_score)
+                    
+            
         print curr_pop
